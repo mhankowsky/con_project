88281cf9735b51c3a63844b3719b3b3add080e20
mnl015 <mnl1615@gmail.com>
2013-06-26 18:05:36 -0400

    modified CSS for main page header and both searchbars

--- MainPagePhp/mainpagefiles/SearchBarMainPage.php
+++ MainPagePhp/mainpagefiles/SearchBarMainPage.php
@@ -5,5 +5,5 @@ include 'style.css';
 <!-- Search Bar -->
 <div id="mainSearchbarDisplay">
     <input id="mainSearchbar" type="search" placeholder="Name of Organization/Person" alt="Search bar Unable to load">
-    <input class="searchbar_submit" type="submit" value="Search">
+    <input class="searchbar_submit" type="submit" value="" >
 </div>
